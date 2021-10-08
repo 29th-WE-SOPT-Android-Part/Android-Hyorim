a27824613debd7496b4245ee223151c814186462
KxxHyoRim <rlagyfla0709@gmail.com>
2021-10-09 04:48:16 +0900

    Complete SignInActivity.kt

--- app/src/main/java/com/hyorim/sopt_assigmnet_1/SignInActivity.kt
+++ app/src/main/java/com/hyorim/sopt_assigmnet_1/SignInActivity.kt
@@ -1,14 +1,40 @@
 package com.hyorim.sopt_assigmnet_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.hyorim.sopt_assigmnet_1.databinding.ActivitySigninBinding
 
 class SignInActivity : AppCompatActivity() {
+    var Tag = "SignInActivity :"
+    private lateinit var binding: ActivitySigninBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.loginBtn.setOnClickListener {
 
+            // ID, PW의 EditText의 null 여부 판단
+            var id = binding.IDEditText.text.toString()
+            var pw = binding.PWEditText.text.toString()
+
+            var isIdNull = id.isNullOrBlank()
+            var isPwNull = pw.isNullOrBlank()
+
+            Log.e(Tag,"isIdNull =" + isIdNull.toString())
+            Log.e(Tag,"isPwNull =" + isPwNull.toString())
+
+            if (!isIdNull && !isPwNull) {   // 둘 다 입력 된 경우
+                Toast.makeText(this, id + "님 환영합니다", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
