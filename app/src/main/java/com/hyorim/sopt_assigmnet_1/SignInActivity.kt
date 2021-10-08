3d2d45438fa23af9c0c986078b7e9eb0529f860e
KxxHyoRim <rlagyfla0709@gmail.com>
2021-10-09 03:05:06 +0900

    Complete activity_signin.xml

--- app/src/main/java/com/hyorim/sopt_assigmnet_1/SignInActivity.kt
+++ app/src/main/java/com/hyorim/sopt_assigmnet_1/SignInActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.e("Test", "Test");
+        setContentView(R.layout.activity_signin)
+
+
     }
 }
