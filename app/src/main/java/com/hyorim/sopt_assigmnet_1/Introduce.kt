4a2dea06d211303725cd352b2acbdf031e1ab48b
KxxHyoRim <rlagyfla0709@gmail.com>
2021-10-31 23:55:32 +0900

    Style : Applying design to HomeActivity.kt

--- app/src/main/java/com/hyorim/sopt_assigmnet_1/Introduce.kt
+++ app/src/main/java/com/hyorim/sopt_assigmnet_1/Introduce.kt
@@ -2,8 +2,7 @@ package com.hyorim.sopt_assigmnet_1
 
 data class Introduce(
     val name: String,
-    var age: Int,
-    var mbti: String,
+    var git_id: String,
     var introducuction: String,
     var photo: Int
 )
