8eaa9a79ce9039265710fce96b728a55c1235f86
KxxHyoRim <rlagyfla0709@gmail.com>
2021-12-12 01:40:19 +0900

    [REFACTOR] Get Image from Gallery on fragment_image.xml using ActivityResultLauncher

--- app/src/main/java/com/hyorim/sopt_assigmnet_1/ImageFragment.kt
+++ app/src/main/java/com/hyorim/sopt_assigmnet_1/ImageFragment.kt
@@ -1,11 +1,14 @@
 package com.hyorim.sopt_assigmnet_1
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.hyorim.sopt_assigmnet_1.databinding.FragmentImageBinding
 
@@ -13,39 +16,42 @@ class ImageFragment : Fragment() {
 
     private var _binding: FragmentImageBinding? = null
     private val binding get() = _binding ?: error("Not Initialized")
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         _binding = FragmentImageBinding.inflate(layoutInflater, container, false)
 
         initClickListener()
+        setImage()
 
         return binding.root
     }
 
-    private fun initClickListener() = binding.btnAppend.setOnClickListener { openGallery() }
+    private fun initClickListener() =
+        binding.btnAppend.setOnClickListener { openGallery() }
 
     private fun openGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = MediaStore.Images.Media.CONTENT_TYPE
-        startActivityForResult(intent, OPEN_GALLERY)
+        resultLauncher.launch(intent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == OPEN_GALLERY) {
-            data?.data.let(binding.ivFromGallery::setImageURI)
-        }
+    private fun setImage() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+                    data?.data.let { binding.ivFromGallery.setImageURI(it) }
+                }
+            }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        const val OPEN_GALLERY = 1      // request code
     }
 }
