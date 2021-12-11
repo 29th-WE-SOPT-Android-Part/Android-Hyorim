63b6456939d5a026ac9d956f17bf447ce1c97bf9
KxxHyoRim <rlagyfla0709@gmail.com>
2021-12-12 02:11:08 +0900

    [FEAT] Set Different Image on RecyclerView using Url

--- app/src/main/java/com/hyorim/sopt_assigmnet_1/BindingConversions.kt
+++ app/src/main/java/com/hyorim/sopt_assigmnet_1/BindingConversions.kt
@@ -9,8 +9,19 @@ import com.bumptech.glide.module.AppGlideModule
 
 object BindingConversions {
 
+    /** follower_list 의 팔로워 프로필 */
     @BindingAdapter("imageUrl")       // Binding Adapter 생성
     @JvmStatic                                   // Static 함수로 설정해주기 위한 Annotation
+    fun loadImage(imageView : ImageView, url : String){
+        Glide.with(imageView.context)
+            .load(url)
+            .circleCrop()
+            .into(imageView)
+    }
+
+    /** fragment_profile 의 개인 프로필 */
+    @BindingAdapter("imageSrc")
+    @JvmStatic
     fun loadImage(imageView : ImageView, url : Int){
         Glide.with(imageView.context)
             .load(url)
--- app/src/main/java/com/hyorim/sopt_assigmnet_1/FollowerData.kt
+++ app/src/main/java/com/hyorim/sopt_assigmnet_1/FollowerData.kt
@@ -1,7 +1,7 @@
 package com.hyorim.sopt_assigmnet_1
 
 data class FollowerData(
-    var photo: Int,
+    var photo: String,
     val name: String,
     val introduction: String
 )
