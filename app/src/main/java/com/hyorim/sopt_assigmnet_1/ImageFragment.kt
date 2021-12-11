90d84025efd4878a7c132a713bef4fcf79e150c9
KxxHyoRim <rlagyfla0709@gmail.com>
2021-12-12 01:20:53 +0900

    [FEAT] Get Image from Gallery on fragment_image.xml

--- app/src/main/java/com/hyorim/sopt_assigmnet_1/ImageFragment.kt
+++ app/src/main/java/com/hyorim/sopt_assigmnet_1/ImageFragment.kt
@@ -1,15 +1,17 @@
 package com.hyorim.sopt_assigmnet_1
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.hyorim.sopt_assigmnet_1.databinding.FragmentImageBinding
 
 class ImageFragment : Fragment() {
 
-    private var _binding : FragmentImageBinding? = null
+    private var _binding: FragmentImageBinding? = null
     private val binding get() = _binding ?: error("Not Initialized")
 
     override fun onCreateView(
@@ -18,11 +20,32 @@ class ImageFragment : Fragment() {
     ): View? {
         _binding = FragmentImageBinding.inflate(layoutInflater, container, false)
 
+        initClickListener()
+
         return binding.root
+    }
+
+    private fun initClickListener() = binding.btnAppend.setOnClickListener { openGallery() }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = MediaStore.Images.Media.CONTENT_TYPE
+        startActivityForResult(intent, OPEN_GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == OPEN_GALLERY) {
+            data?.data.let(binding.ivFromGallery::setImageURI)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val OPEN_GALLERY = 1      // request code
     }
 }
