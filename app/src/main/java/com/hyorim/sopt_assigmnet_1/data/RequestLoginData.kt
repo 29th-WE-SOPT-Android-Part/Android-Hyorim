e87c2774190548f82da2e37089ed6012163497ad
KxxHyoRim <rlagyfla0709@gmail.com>
2021-12-14 09:37:08 +0900

    [REFACTOR] Remove SerializedName on RequestLoginData.kt

--- app/src/main/java/com/hyorim/sopt_assigmnet_1/data/RequestLoginData.kt
+++ app/src/main/java/com/hyorim/sopt_assigmnet_1/data/RequestLoginData.kt
@@ -1,9 +1,6 @@
 package com.hyorim.sopt_assigmnet_1.data
 
-import com.google.gson.annotations.SerializedName
-
 data class RequestLoginData(
-    @SerializedName("email")
     val email : String,
     val password : String
 )
